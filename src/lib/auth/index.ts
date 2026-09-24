49d2c9e5654c658366183f84c6bc67524d5c6052
Soheil Ghanbary <soli.ghanbary@gmail.com>
2026-09-24 11:51:09 +0330

    fix error auth

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -1,8 +1,8 @@
 import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 import { tanstackStartCookies } from 'better-auth/tanstack-start'
-import { db } from '@/db'
-import * as schema from '@/db/schema'
+import { db } from '@/lib/db'
+import * as schema from '@/lib/db/schema'
 
 export const auth = betterAuth({
 	database: drizzleAdapter(db, { provider: 'pg', schema }),
